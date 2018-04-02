8be8c37304bed025ea1275b42277c3344ab012c8
pvs9 <pvs9@list.ru>
2018-04-02 12:30:46 +0300

    Coordinate class

---
+++ Coordinate.py
@@ -0,0 +1,10 @@
+class Coordinate(tuple):
+
+    def __new__(cls, *args, **kwargs):
+        return tuple.__new__(cls)
+
+    def __add__(self, other):
+        return Coordinate(self[0] + other[0], self[1] + other[1])
+
+    def __sub__(self, other):
+        return Coordinate(self[0] - other[0], self[1] - other[1])
